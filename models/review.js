ff764f30d6cea0e27414d488ec9471e107981e14
dtkcs01 <dtkcs01@gmail.com>
2019-01-25 12:51:53 +0530

    Review Schema Updated

---
+++ models/review.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ReviewSchema = new Schema({
+  postedBy : {
+    type : Schema.Types.ObjectId,
+    required : true ,
+     ref : 'user'
+  },
+  text : {
+    type : String,
+    required : true
+  },
+  upVotes : {
+    type: Number,
+    default : 0
+  },
+  downVotes: {
+    type : Number,
+    default : 0
+  },
+  date : {
+    type : Date,
+    required : true,
+    default : Date.now()
+  }
+});
+
+const Review = mongoose.model('review',ReviewSchema);
+
+module.exports = Review;
